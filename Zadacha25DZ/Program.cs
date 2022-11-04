4025d5c6ed23ae1ff6ab2e1c55db8b2307795cf4
Maria <kostroma-mn@yandex.ru>
2022-11-04 22:15:12 +0300

    Zadacha 25 reshena

---
+++ Zadacha25DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача25. Написать цикл, который принимает на вход два числа (А и В) и возводит
+// число А в натуральную степень В.
+
+Console.WriteLine("Введите два числа");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+  
+int step = A;
+   
+    for (int i = 1; i < B; i++)
+    {
+        step = step * A;
+    }
+        
+Console.WriteLine($"stepen = {step}");
